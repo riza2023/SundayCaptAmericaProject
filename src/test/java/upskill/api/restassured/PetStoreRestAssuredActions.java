e390a7ddcaf2ea49105374f28eef0844d91edadc
aaa <aaa@aaas-MBP.fios-router.home>
2023-07-09 13:49:09 -0400

    devRiza

--- src/test/java/upskill/api/restassured/PetStoreRestAssuredActions.java
+++ src/test/java/upskill/api/restassured/PetStoreRestAssuredActions.java
@@ -1,4 +1,8 @@
-package upskill.api.restassured;
+package upskill.api.restassured;   
+
+
+//practicing for GIT to do see the changes, practice
+
 
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
@@ -9,7 +13,7 @@ import java.io.File;
 
 public class PetStoreRestAssuredActions {
 	
-	public void createPets(){
+	public void createPet(){
 		
 		given().
 			body(new File(System.getProperty("user.dir")
